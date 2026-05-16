622a3b93ded3500e31b3209065e8794b00cf8b5f
SumaniqueCode <github.regmisuman@gmail.com>
2026-05-16 21:03:41 +0545

    Partial derivatives

--- mathematics/calculus.py
+++ mathematics/calculus.py
@@ -4,6 +4,15 @@ import sympy as sp
 x = sp.symbols("x")
 f = x**2
 
-# Calculate the derivativeof the function
+# Calculate the derivative of the function
 derivative = sp.diff(f, x)
-print("Derivative of f(x) = x**2:", derivative)
+print("Derivative of f(x) = x**2 is ", derivative)
+
+# partial derivative
+y, z = sp.symbols("y z")
+f= y**2 + 3*z**2 - 4*y*z
+grad_y = sp.diff(f, y)
+grad_z = sp.diff(f, z)
+print("Gradiensts")
+print("Partial derivative with respect to y: ", grad_y)
+print("Partial derivative with respect to z: ", grad_z)
